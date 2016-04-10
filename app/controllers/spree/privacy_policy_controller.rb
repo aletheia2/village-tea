2f7c91c91bc8a9eb93a8ec97ba384e688bbdeada
Brightergy-YakovKaravelov <yakov.karavelov@brightergy.com>
2016-04-10 17:38:29 +0300

    Initial commit

---
+++ app/controllers/spree/privacy_policy_controller.rb
@@ -0,0 +1,8 @@
+module Spree
+  class PrivacyPolicyController < Spree::StoreController
+    respond_to :html
+
+    def index
+    end
+  end
+end
---
+++ app/controllers/spree/store_map_controller.rb
@@ -0,0 +1,8 @@
+module Spree
+  class StoreMapController < Spree::StoreController
+    respond_to :html
+
+    def index
+    end
+  end
+end
